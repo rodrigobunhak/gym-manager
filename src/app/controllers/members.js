62241eb2f7fdbd5628a7d7854a8c64e2fb740259
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-08-09 23:16:14 -0300

    Ordenando dados do banco

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,10 +1,13 @@
 const { age, date } = require('../../lib/utils')
+const Member = require('../models/Member')
 
 module.exports = {
   index(req, res){
 
-    return res.render('members/index')
-
+    Member.all(function(members){
+      return res.render('members/index', { members })
+    })
+    
   },
   create(req, res){
 
@@ -21,17 +24,31 @@ module.exports = {
       }
     }
     
-    return res.redirect("/members")
+    Member.create(req.body, function(member) {
+      return res.redirect(`/members/${member.id}`)
+    })
 
   },
   show(req, res){
 
-    return
+    Member.find(req.params.id, function(member) {
+      if (!member) return res.send("Member not found!")
+
+      member.birth = date(member.birth).birthDay
+    
+      return res.render("members/show", { member })
+    })
 
   },
   edit(req, res){
 
-    return
+    Member.find(req.params.id, function(member) {
+      if (!member) return res.send("Member not found!")
+
+      member.birth = date(member.birth).iso
+    
+      return res.render("members/edit", { member })
+    })
 
   },
   put(req, res){
@@ -44,12 +61,15 @@ module.exports = {
       }
     }
 
-    return
+    Member.update(req.body, function() {
+      return res.redirect(`/members/${req.body.id}`)
+    })
 
   },
   delete(req, res){
 
-    return
-
-  },
+    Member.delete(req.body.id, function() {
+      return res.redirect(`/members/`)
+    })
+  }
 }
