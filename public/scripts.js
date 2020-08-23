900e24e9ada0793a16535d13bcb81bc31d38ac2e
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-08-23 11:34:52 -0300

    Algoritmo de paginação no front II

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,27 @@ for (item of menuItems) {
     item.classList.add("active")
   }
 }
+
+// Paginacao
+// totalPages = 20
+// selectedPage = 15
+
+// [1, ..., 13, 14, 15, 16, 17, ..., 20]
+
+let totalPages = 20,
+    selectedPage = 15
+    pages = []
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+  const pagesBeforeSlectedPage = currentPage >= selectedPage - 2
+  
+
+  if(firstAndLastPage || pagesBeforeSlectedPage && pagesAfterSelectedPage) {
+    pages.push(currentPage)
+  }
+}
+
+console.log(pages)
